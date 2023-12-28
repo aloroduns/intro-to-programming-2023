4376896798cbad17653d57de57e5b3ab5c6b8fe2
mantisdev20 <aloroduns@yahoo.com>
2023-12-28 00:31:30 -0500

    lesson 4.5 updated

--- index.js
+++ index.js
@@ -3,42 +3,16 @@ const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
 
-const skills = [
-    'JavaScript', 'SQL',  'PowerBI'
-];
-
+const skills = ['JavaScript', 'SQL', 'PowerBI'];
 const skillsSection = document.getElementById('skills');
 const skillsList = skillsSection.querySelector('ul');
 
-for (let i = 0; i< skills.length; i++){
+for (let i = 0; i < skills.length; i++) {
     const listItem = document.createElement('li');
     listItem.textContent = skills[i];
-
     skillsList.appendChild(listItem);
 }
-
-var messageSection = document.getElementById("messages");
-var messageList = messageSection.querySelector("ul");
-var newMessage = document.createElement("li");
-
-var userLink = document.createElement("a");
-userLink.href = "mailto:" + usersEmail;
-userLink.textContent = usersName;
-newMessage.appendChild(userLink);
-
-var messageSpan = document.createElement("span");
-messageSpan.textContent = usersMessage;
-newMessage.appendChild(messageSpan);
-
-var removeButton = document.createElement("button");
-removeButton.innerText = "remove";
-removeButton.type = "button";
-newMessage.appendChild(removeButton);
-messageList.appendChild(newMessage);
-removeButton.addEventListener("click", function(){
-    var entry = removeButton.parentNode;
-    entry.remove();
-});
+window.onload = function() {
 var messageForm = document.getElementsByName("leave_message")[0];
 messageForm.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -46,9 +20,32 @@ messageForm.addEventListener("submit", function(event) {
     var email = event.target.usersEmail.value;
     var message = event.target.usersMessage.value;
     console.log(name, email, message);
+
+    var messageSection = document.getElementById("messages");
+    var messageList = messageSection.querySelector("ul");
+    var newMessage = document.createElement("li");
+
+    var userLink = document.createElement("a");
+    userLink.href = "mailto:" + email;
+    userLink.textContent = name;
+    newMessage.appendChild(userLink);
+
+    var messageSpan = document.createElement("span");
+    messageSpan.textContent = message;
+    newMessage.appendChild(messageSpan);
+
+    var removeButton = document.createElement("button");
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function() {
+        var entry = removeButton.parentNode;
+        entry.remove();
+    });
+    newMessage.appendChild(removeButton);
+
+    messageList.appendChild(newMessage);
 });
+}
 
-
-copyright.innerHTML = `Oluwadunsin Ogunleye &copy; ${thisYear}`;
-
+copyright.innerHTML = `Oluwadunsin Ogunleye © ${thisYear}`;
 footer.appendChild(copyright);
